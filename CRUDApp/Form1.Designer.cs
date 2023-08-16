c367f530ca767f9c6355bca7031da8963f660a21
NadaAtef <112628729+Nada-Atef2002@users.noreply.github.com>
2023-08-17 00:27:48 +0300

    Add files via upload

---
+++ CRUDApp/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace CRUDApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.txtempISBN = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtempname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnfind = new System.Windows.Forms.Button();
+            this.txtempid = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnshowall = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 236);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(457, 188);
+            this.dataGridView1.TabIndex = 23;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(342, 137);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(127, 32);
+            this.btnDelete.TabIndex = 22;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(212, 137);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(127, 32);
+            this.btnUpdate.TabIndex = 20;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.Location = new System.Drawing.Point(76, 137);
+            this.btnsave.Margin = new System.Windows.Forms.Padding(2);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(127, 32);
+            this.btnsave.TabIndex = 19;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click_1);
+            // 
+            // txtempISBN
+            // 
+            this.txtempISBN.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtempISBN.Location = new System.Drawing.Point(260, 81);
+            this.txtempISBN.Margin = new System.Windows.Forms.Padding(2);
+            this.txtempISBN.Name = "txtempISBN";
+            this.txtempISBN.Size = new System.Drawing.Size(207, 30);
+            this.txtempISBN.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(66, 83);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 23);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Enter ISBN";
+            // 
+            // txtempname
+            // 
+            this.txtempname.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtempname.Location = new System.Drawing.Point(260, 48);
+            this.txtempname.Margin = new System.Windows.Forms.Padding(2);
+            this.txtempname.Name = "txtempname";
+            this.txtempname.Size = new System.Drawing.Size(207, 30);
+            this.txtempname.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 23);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Enter Book Name";
+            // 
+            // btnfind
+            // 
+            this.btnfind.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnfind.Location = new System.Drawing.Point(343, 6);
+            this.btnfind.Margin = new System.Windows.Forms.Padding(2);
+            this.btnfind.Name = "btnfind";
+            this.btnfind.Size = new System.Drawing.Size(133, 32);
+            this.btnfind.TabIndex = 13;
+            this.btnfind.Text = "Find Book";
+            this.btnfind.UseVisualStyleBackColor = true;
+            this.btnfind.Click += new System.EventHandler(this.btnfind_Click);
+            // 
+            // txtempid
+            // 
+            this.txtempid.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtempid.Location = new System.Drawing.Point(222, 6);
+            this.txtempid.Margin = new System.Windows.Forms.Padding(2);
+            this.txtempid.Name = "txtempid";
+            this.txtempid.Size = new System.Drawing.Size(117, 30);
+            this.txtempid.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 23);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Enter Book Id";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnshowall
+            // 
+            this.btnshowall.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnshowall.Location = new System.Drawing.Point(76, 200);
+            this.btnshowall.Margin = new System.Windows.Forms.Padding(2);
+            this.btnshowall.Name = "btnshowall";
+            this.btnshowall.Size = new System.Drawing.Size(127, 32);
+            this.btnshowall.TabIndex = 24;
+            this.btnshowall.Text = "Show All";
+            this.btnshowall.UseVisualStyleBackColor = true;
+            this.btnshowall.Click += new System.EventHandler(this.btnshowall_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexit.Location = new System.Drawing.Point(343, 200);
+            this.btnexit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(127, 32);
+            this.btnexit.TabIndex = 25;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 444);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnshowall);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.txtempISBN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtempname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnfind);
+            this.Controls.Add(this.txtempid);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.TextBox txtempISBN;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtempname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnfind;
+        private System.Windows.Forms.TextBox txtempid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnshowall;
+        private System.Windows.Forms.Button btnexit;
+    }
+}
+
---
+++ CRUDApp/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CRUDApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        SqlDataReader dr;
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            cn = new SqlConnection("Data Source=SQL5106.site4now.net;" +
+                                   "Initial Catalog=db_a9cf6f_cruddb;" +
+                                   "User Id=db_a9cf6f_cruddb_admin;Password=Mo123456");
+        }
+        private void Clear()
+        {
+            txtempid.Text = "";
+            txtempISBN.Text = "";
+            txtempname.Text = "";
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnsave_Click_1(object sender, EventArgs e)
+        {
+            cn.Open();
+            cmd = new SqlCommand("insert into Books values (@Book_ID , @ISBN , @Name)", cn);
+            cmd.Parameters.AddWithValue("@Book_ID", int.Parse(this.txtempid.Text));
+            cmd.Parameters.AddWithValue("@ISBN", int.Parse(this.txtempISBN.Text));
+            cmd.Parameters.AddWithValue("@Name", this.txtempname.Text);
+            cmd.ExecuteNonQuery();
+            displaydata();
+            Clear();
+            cn.Close();
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            cmd = new SqlCommand($"update Books set Name='{txtempname.Text}', ISBN='{txtempISBN.Text}' where Book_ID='{txtempid.Text}'", cn);
+            cmd.ExecuteNonQuery();
+            displaydata();
+            Clear();
+            cn.Close();
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            cmd = new SqlCommand($"delete from Books where Book_ID='{txtempid.Text}'", cn);
+            cmd.ExecuteNonQuery();
+            displaydata();
+            Clear();
+            cn.Close();
+        }
+        private void displaydata()
+        {
+            cmd = cn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Books";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        private void btnshowall_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            displaydata();
+            Clear();
+            cn.Close();
+        }
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            Application.Exit();
+            cn.Close();
+        }
+        private void btnfind_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            cmd = cn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"select * from Books where Book_ID='{txtempid.Text}'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            Clear();
+            cn.Close();
+        }
+    }
+}
